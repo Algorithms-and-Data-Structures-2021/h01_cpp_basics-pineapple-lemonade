ded65cb0442f33ed37558a5ccacb8fb43005edf9
Georgy Ruzavin <ruzavin02@mail.ru>
2021-03-14 16:42:50 +0300

    all tasks done

--- src/book.cpp
+++ src/book.cpp
@@ -22,15 +22,22 @@ Book::Book(const std::string &title,
   if (authors.empty()) {
     throw std::invalid_argument("Book::authors cannot be empty");
   }
-
-  // Tip 1: остались слезы на щеках, осталось лишь инициализировать поля ...
+  title_ = title;
+  content_ = content;
+  genre_ = genre;
+  publisher_ = publisher;
+  authors_ = authors;
 }
 
 // 2. реализуйте метод ...
 bool Book::AddAuthor(const Author &author) {
-  // здесь мог бы быть ваш сногсшибающий код ...
-  // Tip 1: для поиска дубликатов можно использовать цикл for-each
-  return false;
+  for(const Author& author1 : authors_){
+      if(author1 == author){
+          return false;
+      }
+  }
+  authors_.push_back(author);
+  return true;
 }
 
 // РЕАЛИЗОВАНО
